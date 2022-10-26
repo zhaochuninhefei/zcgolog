5a50dbe4eaa3f5d6b6cebc2ab6d5a3bf88e4e67d
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-10-26 10:52:10 +0800

    修改api注释

--- zclog/api.go
+++ zclog/api.go
@@ -16,115 +16,127 @@ zclog/api.go 提供zcgolog日志输出接口函数
 
 import "fmt"
 
-// 日志级别: DEBUG
+// Print 日志级别: DEBUG
 func Print(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_DEBUG
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
-// 日志级别: DEBUG
+// Printf 日志级别: DEBUG
 func Printf(msg string, params ...interface{}) {
 	msgLogLevel := LOG_LEVEL_DEBUG
 	outputLog(msgLogLevel, msg, params...)
 }
 
-// 日志级别: DEBUG
+// Println 日志级别: DEBUG
 func Println(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_DEBUG
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
+// Debug 输出Debug日志
 func Debug(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_DEBUG
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
+// Debugf 输出Debug日志
 func Debugf(msg string, params ...interface{}) {
 	msgLogLevel := LOG_LEVEL_DEBUG
 	outputLog(msgLogLevel, msg, params...)
 }
 
+// Debugln 输出Debug日志
 func Debugln(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_DEBUG
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
+// Info 输出Info日志
 func Info(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_INFO
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
+// Infof 输出Info日志
 func Infof(msg string, params ...interface{}) {
 	msgLogLevel := LOG_LEVEL_INFO
 	outputLog(msgLogLevel, msg, params...)
 }
 
+// Infoln 输出Info日志
 func Infoln(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_INFO
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
+// Warn 输出Warn日志
 func Warn(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_WARNING
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
+// Warnf 输出Warn日志
 func Warnf(msg string, params ...interface{}) {
 	msgLogLevel := LOG_LEVEL_WARNING
 	outputLog(msgLogLevel, msg, params...)
 }
 
+// Warnln 输出Warn日志
 func Warnln(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_WARNING
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
+// Error 输出Error日志
 func Error(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_ERROR
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
+// Errorf 输出Error日志
 func Errorf(msg string, params ...interface{}) {
 	msgLogLevel := LOG_LEVEL_ERROR
 	outputLog(msgLogLevel, msg, params...)
 }
 
+// Errorln 输出Error日志
 func Errorln(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_ERROR
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
-// 直接输出日志，终止当前goroutine
+// Panic 直接输出日志，终止当前goroutine
 func Panic(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_PANIC
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
-// 直接输出日志，终止当前goroutine
+// Panicf 直接输出日志，终止当前goroutine
 func Panicf(msg string, params ...interface{}) {
 	msgLogLevel := LOG_LEVEL_PANIC
 	outputLog(msgLogLevel, msg, params...)
 }
 
-// 直接输出日志，终止当前goroutine
+// Panicln 直接输出日志，终止当前goroutine
 func Panicln(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_PANIC
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
-// 直接输出日志，终止程序
+// Fatal 直接输出日志，终止程序
 func Fatal(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_FATAL
 	outputLog(msgLogLevel, fmt.Sprint(v...))
 }
 
-// 直接输出日志，终止程序
+// Fatalf 直接输出日志，终止程序
 func Fatalf(msg string, params ...interface{}) {
 	msgLogLevel := LOG_LEVEL_FATAL
 	outputLog(msgLogLevel, msg, params...)
 }
 
-// 直接输出日志，终止程序
+// Fatalln 直接输出日志，终止程序
 func Fatalln(v ...interface{}) {
 	msgLogLevel := LOG_LEVEL_FATAL
 	outputLog(msgLogLevel, fmt.Sprint(v...))
