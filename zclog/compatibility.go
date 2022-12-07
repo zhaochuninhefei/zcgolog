8c87554ae51dc68d427a5390edc71ccd666a01c0
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-12-07 13:34:19 +0800

    对应警告

--- zclog/compatibility.go
+++ zclog/compatibility.go
@@ -19,38 +19,47 @@ import (
 	"log"
 )
 
+//goland:noinspection GoUnusedExportedFunction
 func New(out io.Writer, prefix string, flag int) *log.Logger {
 	return log.New(out, prefix, flag)
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func Default() *log.Logger {
 	return zcgoLogger
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func SetOutput(w io.Writer) {
 	zcgoLogger.SetOutput(w)
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func Flags() int {
 	return zcgoLogger.Flags()
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func SetFlags(flag int) {
 	zcgoLogger.SetFlags(flag)
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func Prefix() string {
 	return zcgoLogger.Prefix()
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func SetPrefix(prefix string) {
 	zcgoLogger.SetPrefix(prefix)
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func Writer() io.Writer {
 	return zcgoLogger.Writer()
 }
 
+//goland:noinspection GoUnusedExportedFunction
 func Output(calldepth int, s string) error {
 	return zcgoLogger.Output(calldepth+1, s) // +1 for this frame.
 }
